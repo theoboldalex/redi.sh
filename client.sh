3480b814d2fc7aafe51ee978d162ef5d26d42fed
theoboldalex <theoboldalex@gmail.com>
2024-05-17 19:15:19 +0100

    Takin out the trash

--- client.sh
+++ client.sh
@@ -1,20 +1,7 @@
 #!/usr/bin/env bash
 
 if [ -z "$1" ]; then
-cat << EOF
-    USAGE: ./client.sh <host> <port>;
-
-    OPTIONS:
-      <host>;    Specify the host to connect to.
-      <port>;    Specify the port number (must be an integer between 1024 and 65535)
-
-    EXAMPLES:
-      ./client.sh localhost 8080   Starts the client on localhost port 8080
-
-    NOTES:
-      - Ensure the port number is not already in use.
-      - Ensure the port number matches that of the server.
-EOF
+    cat ./usage/client.txt
     exit 1
 fi
 
