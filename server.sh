f8da0f585da9bc5002a85293d23567dab1a0e524
theoboldalex <theoboldalex@gmail.com>
2024-05-17 19:03:42 +0100

    Oh god, what have I created?

--- server.sh
+++ server.sh
@@ -5,29 +5,36 @@ HASH_MAP=()
 PORT=$1
 
 if [ -z "$PORT" ]; then
-cat << EOF
+    cat << EOF
     USAGE: ./server.sh <port>;
 
     OPTIONS:
-      <port>;    Specify the port number (must be an integer between 1024 and 65535)
+    <port>;    Specify the port number (must be an integer between 1024 and 65535)
 
     EXAMPLES:
-      ./server.sh 8080   Starts the service on port 8080
-      ./server.sh 443    Starts the service on port 443 (typically used for HTTPS)
+    ./server.sh 8080   Starts the service on port 8080
+    ./server.sh 443    Starts the service on port 443 (typically used for HTTPS)
 
     NOTES:
-      - Ensure the port number is not already in use.
-      - Ports below 1024 require superuser privileges.
-      - Valid port numbers range from 1024 to 65535.
+    - Ensure the port number is not already in use.
+    - Ports below 1024 require superuser privileges.
+    - Valid port numbers range from 1024 to 65535.
 EOF
     exit 1
 fi
 
 source ./utils.sh
 
-# TODO: check port is not blocked or currently in use
-nc -lvk "$PORT" | while IFS= read -r cmd; do
-    if validate_command "$cmd"; then
-        echo "Received input: $cmd"
-    fi
+N_PIPE=/tmp/redish_pipe
+if [[ ! -p $N_PIPE ]]; then
+    mkfifo $N_PIPE
+fi
+
+while true; do
+    cat $N_PIPE |
+    nc -lvk "$PORT" > >(while read -r cmd; do
+        if validate_command "$cmd"; then
+            echo "Received input: $cmd"
+        fi
+    done > $N_PIPE)
 done
