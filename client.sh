bd080522a10e77b3b7a27ffcea079f316e16befb
theoboldalex <theoboldalex@gmail.com>
2024-05-18 20:39:22 +0100

    client can gtf

--- client.sh
+++ client.sh
@@ -1,25 +1,29 @@
 #!/usr/bin/env bash
 
-if [ -z "$1" ]; then
-    cat ./usage/client.txt
-    exit 1
-fi
+# Just run `nc <host> <port>` ensuring port matches the port you run with `./server.sh <port>`
+#
+#
+#
+# if [ -z "$1" ]; then
+#     cat ./usage/client.txt
+#     exit 1
+# fi
 
-HOST=$1
-PORT=$2
+# HOST=$1
+# PORT=$2
 
-source ./utils.sh
+# source ./utils.sh
 
-while true; do
-    echo "Issue a command [GET, SET] (type 'exit' to quit):"
-    read cmd
-    if [ "$cmd" == "exit" ]; then
-        break
-    fi
+# while true; do
+#     echo "Issue a command [GET, SET] (type 'exit' to quit):"
+#     read cmd
+#     if [ "$cmd" == "exit" ]; then
+#         break
+#     fi
 
-    if validate_command "$cmd"; then
-        echo "$cmd" | nc "$HOST" "$PORT"
-    fi
-done
+#     if validate_command "$cmd"; then
+#         echo "$cmd" | nc "$HOST" "$PORT"
+#     fi
+# done
 
 
